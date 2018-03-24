335a666c63a5e3bcf3b8c05d48f3907c4cb28ed9
Franziskus Kiefer <franziskuskiefer@gmail.com>
2018-03-24 21:06:59 +0100

    generate wots test vectors

--- test/wots.c
+++ test/wots.c
@@ -28,11 +28,51 @@ int main()
     randombytes(m, params.n);
     randombytes((unsigned char *)addr, 8 * sizeof(uint32_t));
 
-    printf("Testing WOTS signature and PK derivation.. ");
+    printf("Testing WOTS signature and PK derivation.. \n");
 
+    printf("m: ");
+    for (int i = 0; i < params.n; ++i) {
+        printf("0x%02x, ", m[i]);
+    }
+    printf("\n");
+    printf("seed: ");
+    for (int i = 0; i < params.n; ++i) {
+        printf("0x%02x, ", seed[i]);
+    }
+    printf("\n");
+    printf("addr: ");
+    for (int i = 0; i < 8; ++i) {
+        printf("0x%08x, ", addr[i]);
+    }
+    printf("\n");
     wots_pkgen(&params, pk1, seed, pub_seed, addr);
+    printf("pk1: ");
+    for (int i = 0; i < params.wots_sig_bytes; ++i) {
+        printf("0x%02x, ", pk1[i]);
+    }
+    printf("\n");
     wots_sign(&params, sig, m, seed, pub_seed, addr);
+    printf("sig: ");
+    for (int i = 0; i < params.wots_sig_bytes; ++i) {
+        printf("0x%02x, ", sig[i]);
+    }
+    printf("\n");
+    printf("addr2: ");
+    for (int i = 0; i < 8; ++i) {
+        printf("0x%08x, ", addr[i]);
+    }
+    printf("\n");
     wots_pk_from_sig(&params, pk2, sig, m, pub_seed, addr);
+    printf("pk2: ");
+    for (int i = 0; i < params.wots_sig_bytes; ++i) {
+        printf("0x%02x, ", pk2[i]);
+    }
+    printf("\n");
+    printf("addr3: ");
+    for (int i = 0; i < 8; ++i) {
+        printf("0x%08x, ", addr[i]);
+    }
+    printf("\n");
 
     if (memcmp(pk1, pk2, params.wots_sig_bytes)) {
         printf("failed!\n");
